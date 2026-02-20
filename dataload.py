83c00057b741b529fb7a80c77e3ab62544f3434a
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2026-02-20 00:02:40 -0800

    Capitalize CATEGORY and title case START/STOP

--- dataload.py
+++ dataload.py
@@ -29,6 +29,9 @@ df.drop(df.index[-1], inplace=True)
 df = df.rename(columns=lambda x: x.replace('*', ''))
 df.PURPOSE.fillna('Other', inplace=True)
 df['PURPOSE'] = df.PURPOSE.str.replace('Charity ($)', 'Charity', regex=False)
+df['CATEGORY'] = df.CATEGORY.str.capitalize()
+df['START'] = df.START.str.title()
+df['STOP'] = df.STOP.str.title()
 
 # Address 'Kar?chi' and 'R?walpindi' problems
 df['START'] = df.START.str.replace('Kar?chi', 'Karachi', regex=False)
